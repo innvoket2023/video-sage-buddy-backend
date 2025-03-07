2ad4e8de5ac85482c66ee4113d9d087bbbc7c945
anshravalll <anshraval@gmail.com>
2025-03-07 21:46:27 +0530

    added procfile

--- app.py
+++ app.py
@@ -332,4 +332,5 @@ def query_video():
         return jsonify({"error": "Processing failed"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
